cd661b8ae9e176d1f11ed1fed8ab9f36913ac3fb
anderrf <andersonrochafarias@gmail.com>
2019-05-22 04:01:30 +0900

    Criando insert

--- www/components/banco.js
+++ www/components/banco.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
 
+$(document).on("click", "#cadastrar", function(){
+  var parametros = {
+    "nome": $("#nome").val(),
+    "email": $("#email").val()
+  };
+
+  $.ajax({
+    type: "post", //Como enviar
+    url: "https://bd-first-mobile-andersonrf.c9users.io/webservice/registra.php", //Para onde enviar
+    data: parametros, //O que enviar
+    //Se der certo
+    success: function (data){
+      navigator.notification.alert("Certo:"+data);
+      $("#nome").val("");
+      $("#email").val("");
+    },
+    //Se der errado
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  });
+
+});
