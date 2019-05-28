44d0db41070035a22e40e6d15a7cff77c7d6f318
anderrf <andersonrochafarias@gmail.com>
2019-05-29 04:01:56 +0900

    lista certo, adição dos buttons e inputs

--- www/components/banco.js
+++ www/components/banco.js
@@ -8,7 +8,7 @@ $(document).on("click", "#cadastrar", function(){
 
   $.ajax({
     type: "post", //Como enviar
-    url: "https://bd-first-mobile-andersonrf.c9users.io/webservice/registra.php", //Para onde enviar
+    url: "https://first-bd-mobile-2-andersonrf.c9users.io/webservice/registra.php", //Para onde enviar
     data: parametros, //O que enviar
     //Se der certo
     success: function (data){
@@ -24,6 +24,26 @@ $(document).on("click", "#cadastrar", function(){
 
 });
 
-$(document).on("click", "#lista", function(){
+$(document).on("click", "#btnlista", function(){
   $(location).attr("href", "lista.html");
 });
+
+function listarPessoas(){
+  $.ajax({
+    type: "post", //como enviar
+    url: "https://first-bd-mobile-2-andersonrf.c9users.io/webservice/lista.php", //para onde enviar
+    dataType:"json",
+    //se der certo
+    success: function(data){
+      var itemLista = "";
+      $.each(data.pessoas, function(i, dados){
+        itemLista += "<option value='"+dados.codigo+"'>"+dados.nome+"</option>";
+      });
+      $("#lista").html(itemLista);
+    },
+    //se der errado
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  });
+}
